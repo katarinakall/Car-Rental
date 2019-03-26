fd83d7ab6355beebc289f8b53c14b81cacdc6322
Katarina Käll <katarina_kall86@hotmail.com>
2019-03-26 11:58:28 +0100

    Changed order in file directory

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import StartPage from './main/js/StartPage/StartPage.js';
+import StartPage from './main/js/components/StartPage.js';
 
 class App extends Component {
   render() {
